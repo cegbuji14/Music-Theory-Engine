dd0ac68eae287b0bb4a74675158319ed35e98e3f
cegbuji14 <57194902+cegbuji14@users.noreply.github.com>
2026-02-02 20:16:00 -0600

    Centered circle of fifths

--- frontend/MusicTheoryEngine/src/App.tsx
+++ frontend/MusicTheoryEngine/src/App.tsx
@@ -4,12 +4,15 @@ import CircleOfFifths from "./components/CircleOfFifths";
 
 function App() {
   return (
-    <div style={{ padding: "40px" }}>
+    <div id="app-wrapper" style={{ height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
       <h1>Circle of Fifths</h1>
       <CircleOfFifths />
     </div>
   );
 }
+
+
+
 
 export default App;
 
