1dbee725ca81de3e6148c87c25ef5a9fae4c0da7
cegbuji14 <57194902+cegbuji14@users.noreply.github.com>
2026-02-02 18:46:46 -0600

    Added react and svg frontend circle

---
+++ frontend/MusicTheoryEngine/src/App.tsx
@@ -0,0 +1,14 @@
+import CircleOfFifths from "./components/CircleOfFifths";
+
+function App() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Circle of Fifths</h1>
+      <CircleOfFifths />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ frontend/MusicTheoryEngine/src/components/CircleOfFifths.tsx
@@ -0,0 +1,17 @@
+const CircleOfFifths = () => {
+    return (
+      <svg width="400" height="400" viewBox="0 0 400 400">
+        <circle
+          cx="200"
+          cy="200"
+          r="150"
+          stroke="black"
+          strokeWidth="2"
+          fill="none"
+        />
+      </svg>
+    );
+  };
+  
+  export default CircleOfFifths;
+  
